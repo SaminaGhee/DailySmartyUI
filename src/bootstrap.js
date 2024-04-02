b195f0d55f22869609bb4fa388ba990e09768536
SaminaGreen <samantha.green91@outlook.com>
2024-04-02 14:50:27 -0500

    put together fetchPostsWithQuery action to receive queried posts

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,36 +1,69 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import thunk from 'redux-thunk';
 
-import rootReducers from "./reducers";
+import reducers from './reducers';
 
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
-const createStoreWithMiddleware = createStore( rootReducers, applyMiddleware(thunk)
-// const  createStoreWithMiddleware = applyMiddleware(ReduxThunk)
-(compose((window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore))));
+// import 'bootstrap/dist/css/bootstrap.css';
+import './style/main.scss';
 
-import "./style/main.scss";
-
-import Home from "./components/home";
-import Results from "./components/results.js";
+import Home from './components/home';
+import Results from './components/results';
 
 function main() {
   ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(rootReducers)}>
+    <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
         <Switch>
-          <Route path="/" exact component={Home}/>
-          <Route path='/results' component={Results}/>
+            <Route path='/' exact component={Home}/>
+            <Route path='/results' component={Results}/>
         </Switch>
       </BrowserRouter>
-    </Provider>,
-    document.querySelector(".app-wrapper")
-  );
+    </Provider>
+    , document.querySelector('.app-wrapper'));
 }
 
-document.addEventListener("DOMContentLoaded", main);
-export default rootReducers;
+document.addEventListener('DOMContentLoaded', main);
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { Provider } from "react-redux";
+// import { createStore, applyMiddleware, compose } from "redux";
+// import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+// import thunk from 'redux-thunk';
+
+// import rootReducers from "./reducers";
+
+
+// const createStoreWithMiddleware = createStore( rootReducers, applyMiddleware(thunk)
+// // const  createStoreWithMiddleware = applyMiddleware(ReduxThunk)
+// (compose((window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore))));
+
+// import "./style/main.scss";
+
+// import Home from "./components/home";
+// import Results from "./components/results.js";
+
+// function main() {
+//   ReactDOM.render(
+//     <Provider store={createStoreWithMiddleware(rootReducers)}>
+//       <BrowserRouter>
+//         <Switch>
+//           <Route path="/" exact component={Home}/>
+//           <Route path='/results' component={Results}/>
+//         </Switch>
+//       </BrowserRouter>
+//     </Provider>,
+//     document.querySelector(".app-wrapper")
+//   );
+// }
+
+// document.addEventListener("DOMContentLoaded", main);
+// export default rootReducers;
--- src/components/home.js
+++ src/components/home.js
@@ -3,11 +3,13 @@ import React, { Component } from 'react';
 import Logo from './logo';
 import SearchBar from './searchbar';
 import RecentPosts from './recentposts';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-export default class Home extends Component {
+class Home extends Component {
 
     handleSearchBarSubmit(query) {
-      console.log('trying to handle submit for query', query);
+      this.props.fetchPostsWithQuery(query);
       this.props.history.push('/results');
   }
 
@@ -23,3 +25,4 @@ export default class Home extends Component {
     );
   }
 }
+export default connect(null, actions)(Home);
